ff7f9de8ec90a9db21d7c229db5ed43eb33fc08c
Phoenix Zhang <phoenix.nir@gmail.com>
2017-02-24 18:12:16 +0800

    ifx

--- project.clj
+++ project.clj
@@ -7,11 +7,11 @@
                  [incanter/incanter-core "1.5.7"]
                  [org.clojure/data.csv "0.1.3"]
                  [com.opencsv/opencsv "3.8"]
-                 [mount "0.1.10"]
+                 [mount "0.1.11"]
                  [cprop "0.1.9"]
                  [org.clojure/tools.cli "0.3.5"]
                  [org.clojure/tools.logging "0.3.1"]
                  [ch.qos.logback/logback-classic "1.1.7"]]
-  :main ^:skip-aot hs300s-analysis.core
+  :main hs300s-analysis.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
